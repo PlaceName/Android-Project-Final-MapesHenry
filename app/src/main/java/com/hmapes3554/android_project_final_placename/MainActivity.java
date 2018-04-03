1e91d6a1b6523356dc760c13e94cc16187ec109a
hmapes3554 <henryaamapes@gmail.com>
2018-04-03 14:54:35 -0700

    Initial commit

---
+++ app/src/main/java/com/hmapes3554/android_project_final_placename/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hmapes3554.android_project_final_placename;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
